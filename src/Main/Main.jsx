5cb2a9dd092901ff1cea0c8e3abdde0241ef80d7
Evgenij <orkinevgenij@gmail.com>
2022-02-06 17:40:48 +0200

    wrap

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -7,16 +7,16 @@ const Main = (props) => {
       <div className="row">
         <article className="col col__article">
 
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div style={{ display: 'flex', justifyContent: 'space-between', flexWrap:"wrap"}}>
             <div>
               <img
                 src="https://www.monobank.com.ua/resources/1.0.16.1-1643818182000/img/main/home-main-iphone-uk.webp"
                 alt="credit_100000"/>
             </div>
-            <div >
+            <div>
               <h4>Дополнительная информация о пользователе:</h4>
-              {userInfo.map((info,index) => {
-                return <div key={index}  style={{fontSize:'18px'}}>
+              {userInfo.map((info, index) => {
+                return <div key={index} style={{ fontSize: '18px' }}>
                   <p>{info.login}</p>
                   <p>{info.id}</p>
                   <p>{info.node_id}</p>
